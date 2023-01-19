023a63cdcfe3b1defe7fa4ef8843b3a1d495c522
Каныкей Алишерова <kanykejaliserova@MacBook-Pro-Kanykej.local>
2023-01-19 17:28:43 +0600

    last

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,28 +1,41 @@
-// import React from "react";
-// import { Route, Routes } from "react-router-dom";
-// import HomePage from "../pages/HomaPage/HomePage";
-// import MedicinePage from "../pages/medicine/MedicinePage";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Children from "../components/ children/ Children";
+import NursingHouse from "../components/nursinghouse/NursingHouse";
+import HomePage from "../pages/HomaPage/HomePage";
+import MedicinePage from "../pages/medicine/MedicinePage";
 
-// const MainRoutes = () => {
-//   const PUBLIC_ROUTES = [
-//     {
-//       link: "/",
-//       element: <HomePage />,
-//       id: 1,
-//     },
-//     {
-//       link: "/medicine",
-//       element: <MedicinePage />,
-//       id: 2,
-//     },
-//   ];
-//   return (
-//     <Routes>
-//       {PUBLIC_ROUTES.map((page) => (
-//         <Route path={page.link} element={page.element} key={page.id} />
-//       ))}
-//     </Routes>
-//   );
-// };
+const MainRoutes = () => {
+  const PUBLIC_ROUTES = [
+    {
+      link: "/",
+      element: <HomePage />,
+      id: 1,
+    },
+    {
+      link: "/medicine",
+      element: <MedicinePage />,
+      id: 2,
+    },
+    {
+      link: "/deti",
+      element: <Children />,
+      id: 3,
+    },
 
-// export default MainRoutes;
+    {
+      link: "/nursingHouse",
+      element: <NursingHouse />,
+      id: 5,
+    },
+  ];
+  return (
+    <Routes>
+      {PUBLIC_ROUTES.map((page) => (
+        <Route path={page.link} element={page.element} key={page.id} />
+      ))}
+    </Routes>
+  );
+};
+
+export default MainRoutes;
